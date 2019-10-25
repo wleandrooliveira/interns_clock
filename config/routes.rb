d519d7c47249be4d75c995b02dd6570e68cc3719
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2019-10-25 03:10:06 -0300

    Final user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'pages/home'
 
   root 'application#hello'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
